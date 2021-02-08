564a99876de612d54fb2178e3b810401ee3edc76
todd <todd+github@goneopen.com>
2021-02-09 07:01:08 +1300

    type refactor

--- src/linkRelation.ts
+++ src/linkRelation.ts
@@ -1,12 +1,10 @@
-import { RelationshipType } from 'semantic-link';
-
 export default class LinkRelation {
-    static readonly Self: RelationshipType = 'self';
-    static readonly Canonical: RelationshipType = 'canonical';
-    static readonly Up: RelationshipType = 'up';
-    static readonly Alternate: RelationshipType = 'alternate';
-    static readonly EditForm: RelationshipType = 'edit-form';
-    static readonly CreateForm: RelationshipType = 'create-form';
-    static readonly SearchForm: RelationshipType = 'search-form';
-    static readonly Submit: RelationshipType = 'submit';
+    static readonly Self = 'self';
+    static readonly Canonical = 'canonical';
+    static readonly Up = 'up';
+    static readonly Alternate = 'alternate';
+    static readonly EditForm = 'edit-form';
+    static readonly CreateForm = 'create-form';
+    static readonly SearchForm = 'search-form';
+    static readonly Submit = 'submit';
 }
